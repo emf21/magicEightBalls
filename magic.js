37f93f9f1eb45fc4112e88a77ebc5956decc3eb1
emf21 <emefa.ameku@amalitech.org>
2021-12-08 13:54:13 +0000

    Magic eight ball project

---
+++ magic.js
@@ -0,0 +1,39 @@
+// Magic eight balls
+// This project helps check control flow
+
+let userName = "";
+userName ? console.log(`Hello ${userName}!`) : console.log("Hello!");
+
+let userQuestion = "Provide answer to this section";
+console.log(`The user asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+switch (randomNumber) {
+  case 0:
+    eightBall = "Do not count on it";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "My sources say no";
+    break;
+  case 5:
+    eightBall = "It is certain";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+}
+console.log(`The eightBall answered: ${eightBall}`);
